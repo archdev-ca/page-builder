029cff15c2214515304ee992c2444f47a5dd7ff4
Archie G <acgelonga@gmail.com>
2022-10-20 21:03:30 -0500

    WIP: iterating through state

--- src/components/Droppable/index.tsx
+++ src/components/Droppable/index.tsx
@@ -2,11 +2,16 @@ import * as React from "react";
 import { useDrop } from "react-dnd";
 import "./style.scss";
 
-type DroppableProps = {};
+type DroppableProps = {
+  idCounter: React.RefObject<number>;
+  id: number;
+  component?: React.ReactNode;
+};
 
-const Droppable = ({}: DroppableProps) => {
+const Droppable = ({ id, idCounter }: DroppableProps) => {
   const handleDrop = (item: any, monitor: any) => {
-    console.log("handleDrop: ", item, monitor);
+    console.log("handleDrop: ", id);
+    console.log("handleDrop: monitor", monitor.getDropResult());
     return {};
   };
 
--- src/pages/Builder/index.tsx
+++ src/pages/Builder/index.tsx
@@ -10,13 +10,22 @@ import "./style.scss";
 type BuilderProps = {};
 
 const Builder = ({}: BuilderProps) => {
+  let idCounter = React.useRef(1);
+  const [nodes, setNodes] = React.useState({
+    byId: {
+      0: { id: 0 },
+    },
+    tree: [{ id: 0 }],
+    allIds: [0],
+  });
+
   return (
     <Container fluid>
       <Row>
         <DndProvider backend={HTML5Backend}>
           <Col className="d-flex">
             <div className="sidebar py-4">
-              <ComponentCard key={1} type="col">
+              <ComponentCard key={1} type="nav">
                 Nav
               </ComponentCard>
               <ComponentCard key={2} type="col">
@@ -24,7 +33,8 @@ const Builder = ({}: BuilderProps) => {
               </ComponentCard>
             </div>
             <div className="main-col flex-grow">
-              <Droppable></Droppable>
+              <Droppable id={0} idCounter={idCounter}></Droppable>
+              {nodes.tree.map((obj) => {})}
             </div>
           </Col>
         </DndProvider>
