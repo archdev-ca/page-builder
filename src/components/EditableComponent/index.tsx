c3f4627f3c239607e05846f446720e6ac4ecc236
Archie G <acgelonga@gmail.com>
2022-10-20 21:03:13 -0500

    Create EditableComponent

---
+++ src/components/EditableComponent/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./style.scss";
+
+type EditableComponentProps = {};
+
+const EditableComponent = ({}: EditableComponentProps) => {
+  return <div>Editable Component</div>;
+};
+
+export default EditableComponent;
